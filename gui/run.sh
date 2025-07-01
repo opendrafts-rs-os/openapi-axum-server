123f1e0ba95eec64b9b1b57a699e36e801398c5e
Grzegorz Bylica <grzegorz.bylica@gmail.com>
2025-07-02 00:45:42 +0200

    wip: init auth0 part2 gui and bk

--- gui/run.sh
+++ gui/run.sh
@@ -8,8 +8,9 @@ set +o allexport
 
 trunk serve \
     --address 127.0.0.1 \
-    --port 8080 \
-    --tls-cert-path ./certs/cert.pem \
-    --tls-key-path ./certs/key.pem
+    --port 8080
+#    \
+#    --tls-cert-path ./certs/cert.pem \
+#    --tls-key-path ./certs/key.pem
 
 # trunk serve --host localhost --tls-key-path ./certs/key.pem --tls-cert-path ./certs/cert.pem
